e10e10647f986a2b93b62fa49b57dd5943d791e3
AlexSkrytski <alcoreservice@gmail.com>
2026-05-19 17:47:18 +0300

    HW3

--- LoginPasswordVerification()/Program.cs
+++ LoginPasswordVerification()/Program.cs
@@ -1,13 +1,12 @@
-﻿namespace LoginPasswordVerification__
+﻿namespace HomeWork3
 {
-    internal class Program
+    class Program
     {
         static void Main()
         {
-            LoginPasswordVerification();
+           // LoginPasswordVerification();
             VowelsCounter();
-            //SalaryBYHours();
-
+           // ScoresGrateCheck();
         }
         static void LoginPasswordVerification()
         {
@@ -46,9 +45,9 @@
                 Console.WriteLine("No more attempts left!");
             }
         }
-
         static void VowelsCounter()
         {
+
             Console.WriteLine("Введите текст:");
             string text = Console.ReadLine() ?? string.Empty; ;
             string lowerText = text.ToLower();
@@ -57,30 +56,35 @@
 
             char[] vawelsArray = { 'а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'з', 'ю', 'я' };
 
-            foreach (char ch in lowerText)
+            foreach (char stringItem in lowerText)
             {
                 for (int i = 0; i < vawelsArray.Length - 1; i++)
                 {
-                    if (ch == vawelsArray[i])
+                    if (stringItem == vawelsArray[i])
                     {
                         vowelsCount++;
                     }
                 }
-
-                //Variant 2
-                //string vowels  = "аеёиоуызюя";
-                //int vowelsCount = 0;
-
-                //foreach (char ch in lowerText)
-                //{
-                //    if (vowels.Contains(ch))
-                //    {
-                //        vowelsCount++;
-                //    }
-                //}
-
             }
-            Console.WriteLine($"Всего гласных: {vowelsCount}");
+
+            Console.WriteLine($"Всего гласных: {vowelsCount}.");
+
+        }
+        static void ScoresGrateCheck()
+        {
+            int score = 80;
+
+            string grade = score switch
+            {
+                >= 90 => "A",
+                >= 80 => "B",
+                >= 70 => "C",
+                >= 60 => "D",
+                _ => "F"
+            };
+
+            Console.WriteLine(grade);
+
         }
     }
 }
